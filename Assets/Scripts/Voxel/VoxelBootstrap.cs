1cee68d92fe6021c8d4f613d9d9d06dc1df7afdb
NotWhiskey12138 <137181834+NotWhiskey12138@users.noreply.github.com>
2026-04-17 22:19:54 -0700

    Add Destroy
    Now u can press r to destroy voxel at player position with radius.

--- Assets/Scripts/Voxel/VoxelBootstrap.cs
+++ Assets/Scripts/Voxel/VoxelBootstrap.cs
@@ -5,7 +5,12 @@ public class VoxelBootstrap : MonoBehaviour
 {
     [SerializeField] private VoxelWorld world;
     [SerializeField] private ChunkViewManager viewManager;
+    [SerializeField] private Transform player;
+    [SerializeField] private int radius;
 
+    private int playerPosX;
+    private int playerPosY;
+    private int playerPosZ;
     private void Start()
     {
         BuildMinimalWorld();
@@ -13,9 +18,13 @@ public class VoxelBootstrap : MonoBehaviour
 
     public void Update()
     {
+        playerPosX = (int)player.position.x;
+        playerPosY = (int)player.position.y;
+        playerPosZ = (int)player.position.z;
+        
         if (Input.GetKey(KeyCode.R))
         {
-            world.updateChunk(new Vector3Int(0, 10, 0), 10);
+            world.updateChunk(new Vector3Int(playerPosX,playerPosY,playerPosZ), radius);
             viewManager.Rebuild();
         }
     }
